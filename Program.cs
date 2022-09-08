1ce7b2ec2f72fdcb70c75be1a4058fcdc246b67e
mandara11 <mandaram2345@gmail.com>
2022-09-08 13:21:55 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeePayrollSystemThreads
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll System Using Threads.");
+
+        }
+
+    }
+}
